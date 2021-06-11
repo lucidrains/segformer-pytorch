f5772bf95c1f6d30b6b125169d058dcbaf146022
Phil Wang <lucidrains@gmail.com>
2021-06-11 01:37:10 -0700

    complete segformer

--- README.md
+++ README.md
@@ -4,6 +4,38 @@
 
 Implementation of <a href="https://arxiv.org/abs/2105.15203">Segformer</a>, Attention + MLP neural network for segmentation, in Pytorch.
 
+## Install
+
+```bash
+$ pip install segformer-pytorch
+```
+
+## Usage
+
+For example, MiT-B0
+
+```python
+import torch
+from segformer_pytorch import Segformer
+
+model = Segformer(
+    image_size = 256,               # image size
+    patch_size = 4,                 # patch size
+    dims = (32, 64, 160, 256),      # dimensions of each stage
+    heads = (1, 2, 5, 8),           # heads of each stage
+    ff_expansion = (8, 8, 4, 4),    # feedforward expansion factor of each stage
+    reduction_ratio = (8, 4, 2, 1), # reduction ratio of each stage for efficient attention
+    num_layers = 2,                 # num layers of each stage
+    decoder_dim = 256,              # decoder dimension
+    num_classes = 4                 # number of segmentation classes
+)
+
+x = torch.randn(1, 3, 256, 256)
+pred = model(x) # (1, 4, 64, 64)  # output is (H/4, W/4) map of the number of segmentation classes
+```
+
+Make sure the keywords are at most a tuple of 4, as this repository is hard-coded to give the MiT 4 stages as done in the paper.
+
 ## Citations
 
 ```bibtex
--- segformer_pytorch/segformer_pytorch.py
+++ segformer_pytorch/segformer_pytorch.py
@@ -1,39 +1,183 @@
+from functools import partial
 import torch
 from torch import nn, einsum
 import torch.nn.functional as F
 
-from einops import rearrange
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
 
 # helpers
 
 def exists(val):
     return val is not None
 
+def cast_tuple(val, depth):
+    return val if isinstance(val, tuple) else (val,) * depth
+
+LayerNorm = partial(nn.InstanceNorm2d, affine = True)
+
 # classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = LayerNorm(dim)
+
+    def forward(self, x):
+        return self.fn(self.norm(x))
+
+class EfficientSelfAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads,
+        reduction_ratio
+    ):
+        super().__init__()
+        self.scale = (dim // heads) ** -0.5
+        self.heads = heads
+        self.reduction_ratio = reduction_ratio
+
+        self.to_qkv = nn.Conv2d(dim, dim * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(dim, dim, 1, bias = False)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        heads, r = self.heads, self.reduction_ratio
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = 1)
+        k, v = map(lambda t: reduce(t, 'b c (h r1) (w r2) -> b c h w', 'mean', r1 = r, r2 = r), (k, v))
+
+        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> (b h) (x y) c', h = heads), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) (x y) c -> b (h c) x y', h = heads, x = h, y = w)
+        return self.to_out(out)
+
+class MixFeedForward(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        expansion_factor
+    ):
+        super().__init__()
+        hidden_dim = dim * expansion_factor
+        self.net = nn.Sequential(
+            nn.Conv2d(dim, hidden_dim, 1),
+            nn.Conv2d(hidden_dim, hidden_dim, 3, padding = 1),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, dim, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 class MiT(nn.Module):
     def __init__(
         self,
         *,
-        dim,
-        heads = 8,
-        dim_head = 64
+        image_size,
+        channels,
+        dims,
+        heads,
+        ff_expansion,
+        reduction_ratio,
+        num_layers
     ):
         super().__init__()
+        stage_kernel_stride_pad = ((7, 4, 3), (3, 2, 1), (3, 2, 1), (3, 2, 1))
 
-    def forward(self, x):
-        return x
+        dims = (channels, *dims)
+        dim_pairs = list(zip(dims[:-1], dims[1:]))
+
+        self.stages = nn.ModuleList([])
+
+        for (dim_in, dim_out), (kernel, stride, padding), num_layers, ff_expansion, heads, reduction_ratio in zip(dim_pairs, stage_kernel_stride_pad, num_layers, ff_expansion, heads, reduction_ratio):
+            image_size //= stride
+
+            overlap_patch_embed = nn.Sequential(
+                nn.Unfold(kernel, stride = stride, padding = padding),
+                Rearrange('b c (h w) -> b c h w', h = image_size, w = image_size),
+                nn.Conv2d(dim_in * kernel ** 2, dim_out, 1),
+            )
+
+            layers = nn.ModuleList([])
+
+            for _ in range(num_layers):
+                layers.append(nn.ModuleList([
+                    PreNorm(dim_out, EfficientSelfAttention(dim = dim_out, heads = heads, reduction_ratio = reduction_ratio)),
+                    PreNorm(dim_out, MixFeedForward(dim = dim_out, expansion_factor = ff_expansion)),
+                ]))
+
+            self.stages.append(nn.ModuleList([
+                overlap_patch_embed,
+                layers
+            ]))
+
+    def forward(
+        self,
+        x,
+        return_layer_outputs = False
+    ):
+        layer_outputs = []
+        for (overlap_embed, layers) in self.stages:
+            x = overlap_embed(x)
+            for (attn, ff) in layers:
+                x = attn(x) + x
+                x = ff(x) + x
+            layer_outputs.append(x)
+
+        ret = x if not return_layer_outputs else layer_outputs
+        return ret
 
 class Segformer(nn.Module):
     def __init__(
         self,
         *,
-        dim,
-        heads = 8,
-        num_classes = 4,
-        dim_head = 64
+        image_size,
+        patch_size = 4,
+        dims = (32, 64, 160, 256),
+        heads = (1, 2, 5, 8),
+        ff_expansion = (8, 8, 4, 4),
+        reduction_ratio = (8, 4, 2, 1),
+        num_layers = 2,
+        channels = 3,
+        decoder_dim = 256,
+        num_classes = 4
     ):
         super().__init__()
+        dims, heads, ff_expansion, reduction_ratio, num_layers = map(partial(cast_tuple, depth = 4), (dims, heads, ff_expansion, reduction_ratio, num_layers))
+        assert all([*map(lambda t: len(t) == 4, (dims, heads, ff_expansion, reduction_ratio, num_layers))]), 'only four stages are allowed, all keyword arguments must be either a single value or a tuple of 4 values'
+
+        self.mit = MiT(
+            image_size = image_size,
+            channels = channels,
+            dims = dims,
+            heads = heads,
+            ff_expansion = ff_expansion,
+            reduction_ratio = reduction_ratio,
+            num_layers = num_layers
+        )
+
+        self.to_fused = nn.ModuleList([nn.Sequential(
+            nn.Conv2d(dim, decoder_dim, 1),
+            nn.Upsample(scale_factor = 2 ** i)
+        ) for i, dim in enumerate(dims)])
+
+        self.to_segmentation = nn.Sequential(
+            nn.Conv2d(4 * decoder_dim, decoder_dim, 1),
+            nn.Conv2d(decoder_dim, num_classes, 1),
+        )
 
     def forward(self, x):
-        return x
+        layer_outputs = self.mit(x, return_layer_outputs = True)
+
+        fused = [to_fused(output) for output, to_fused in zip(layer_outputs, self.to_fused)]
+        fused = torch.cat(fused, dim = 1)
+        return self.to_segmentation(fused)
