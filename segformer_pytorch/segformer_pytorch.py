5868d8e75b2360bf26a83bd03f2a8ec3d9b1e8f7
Phil Wang <lucidrains@gmail.com>
2021-06-23 09:39:02 -0700

    use depth-wise conv

--- segformer_pytorch/segformer_pytorch.py
+++ segformer_pytorch/segformer_pytorch.py
@@ -19,6 +19,16 @@ LayerNorm = partial(nn.InstanceNorm2d, affine = True)
 
 # classes
 
+class DsConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, kernel_size, padding, stride = 1, bias = True):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(dim_in, dim_in, kernel_size = kernel_size, padding = padding, groups = dim_in, stride = stride, bias = bias),
+            nn.Conv2d(dim_in, dim_out, kernel_size = 1, bias = bias)
+        )
+    def forward(self, x):
+        return self.net(x)
+
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -69,7 +79,7 @@ class MixFeedForward(nn.Module):
         hidden_dim = dim * expansion_factor
         self.net = nn.Sequential(
             nn.Conv2d(dim, hidden_dim, 1),
-            nn.Conv2d(hidden_dim, hidden_dim, 3, padding = 1),
+            DsConv2d(hidden_dim, hidden_dim, 3, padding = 1),
             nn.GELU(),
             nn.Conv2d(hidden_dim, dim, 1)
         )
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'segformer-pytorch',
   packages = find_packages(),
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'Segformer - Pytorch',
   author = 'Phil Wang',
