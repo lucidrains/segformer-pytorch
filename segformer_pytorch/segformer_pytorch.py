b49efeae7b4fda4f9daf01e8efab91edcc9f42f0
Phil Wang <lucidrains@gmail.com>
2021-08-24 08:22:18 -0700

    fix layernorm

--- segformer_pytorch/segformer_pytorch.py
+++ segformer_pytorch/segformer_pytorch.py
@@ -15,8 +15,6 @@ def exists(val):
 def cast_tuple(val, depth):
     return val if isinstance(val, tuple) else (val,) * depth
 
-LayerNorm = partial(nn.InstanceNorm2d, affine = True)
-
 # classes
 
 class DsConv2d(nn.Module):
@@ -28,6 +26,18 @@ class DsConv2d(nn.Module):
         )
     def forward(self, x):
         return self.net(x)
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        std = torch.var(x, dim = 1, unbiased = False, keepdim = True).sqrt()
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) / (std + self.eps) * self.g + self.b
 
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
